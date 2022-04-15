6d1c105c748cce7c2640cd104b671bb30dff502b
Karl Boyle <karlboyle@gmail.com>
2022-04-15 15:03:39 +0100

    Initial Commit

---
+++ jhealth-core/src/main/java/com/github/karlboyle/jhealthcore/JhealthCoreApplication.java
@@ -0,0 +1,12 @@
+package com.github.karlboyle.jhealthcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhealthCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JhealthCoreApplication.class, args);
+    }
+}
---
+++ jhealth-core/src/test/java/com/github/karlboyle/jhealthcore/JhealthCoreApplicationTests.java
@@ -0,0 +1,11 @@
+package com.github.karlboyle.jhealthcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JhealthCoreApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
